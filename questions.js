7e1d84584ba7fecaa2d9227db54a25fc5899a896
Julia Romenia <124843205+juliaromenia@users.noreply.github.com>
2023-07-24 21:18:56 -0300

    Projeto final curso potencia feminina

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Qual foi a contribuição de Ada Lovelace para a história da computação?",
+      answers: [
+        { option: "Criou o primeiro algoritmo de programação", correct: true },
+        { option: "Desenvolveu o primeiro computador eletrônico ", correct: false },
+        { option: "Escreveu o primeiro livro sobre computação", correct: false },
+      ],
+    },
+    {
+      question: "Qual era a profissão de Ada Lovelace?",
+      answers: [
+        { option: "Astrônoma", correct: false },
+        { option: "Matemática ", correct: true },
+        { option: "Advogada", correct: false },
+      ],
+    },
+    {
+      question: "Ada Lovelace é conhecida como:",
+      answers: [
+        { option: "A primeira engenheira de software", correct: false },
+        { option: "A primeira cientista da computação", correct: false },
+        { option: "A primeira programadora", correct: true },
+      ],
+    },
+    {
+      question: "Em que ano foi aprovada a linguagem de programação ADA?",
+      answers: [
+        { option: "1970", correct: false },
+        { option: "1990", correct: false },
+        { option: "1980", correct: true },
+      ],
+    },
+  ];
